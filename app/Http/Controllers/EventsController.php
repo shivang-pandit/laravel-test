3cc5906d213a75333d2900780d83aa1c32113058
Shivang <shivang512@gmail.com>
2022-06-01 22:15:04 +0530

    Endpoint to get event with webshop

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -101,7 +101,9 @@ class EventsController extends BaseController
      */
 
     public function getEventsWithWorkshops() {
-        throw new \Exception('implement in coding task 1');
+        //throw new \Exception('implement in coding task 1');
+        $events = Event::with('workShops')->get();
+        return response()->json($events,200);
     }
 
 
